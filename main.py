169161d253bf7a6249ab331215646223ab0b1b85
Ryo <jukenohtani@gmail.com>
2024-09-16 12:52:36 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+
+def hello():
+    return "Hello, World!"
+
+
+def example_function():
+    arr = np.array([1, 2, 3, 4, 5])
+    return arr.mean()
---
+++ test.py
@@ -0,0 +1,16 @@
+import unittest
+from main import *
+
+
+class TestHello(unittest.TestCase):
+    def test_main(self):
+        self.assertEqual(hello(), "Hello, World!")
+
+
+class TestNumpy(unittest.TestCase):
+    def test_example_function(self):
+        self.assertEqual(example_function(), 3.0)
+
+
+if __name__ == '__main__':
+    unittest.main()
